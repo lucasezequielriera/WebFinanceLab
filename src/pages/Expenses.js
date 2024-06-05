f14b7ee27b34559871d178f8495fa368a22463de
Lucas Riera <lucasezequielriera@hotmail.com>
2024-06-05 13:51:08 -0300

    Buttons styles for detailed and general expenses

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Spin } from 'antd';
 import CreditCard from '../components/CreditCard';
-import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useAuth } from '../contexts/AuthContext';
 import moment from 'moment';
@@ -20,41 +20,6 @@ const Expenses = () => {
   const [loading, setLoading] = useState(true);
   const [cards, setCards] = useState([]);
   const navigate = useNavigate();
-
-  const fetchCards = async () => {
-    if (!currentUser) return;
-
-    const userDocRef = doc(db, "users", currentUser.uid);
-    const userDoc = await getDoc(userDocRef);
-    if (userDoc.exists()) {
-      const userData = userDoc.data();
-      const cardsData = [];
-      if (userData.creditCards) {
-        const cardMap = new Map();
-
-        userData.creditCards.forEach((card) => {
-          const key = `${card.bank}-${card.cardBank}-${card.cardType}`;
-          if (cardMap.has(key)) {
-            const existingCard = cardMap.get(key);
-            existingCard.amount += card.amount;
-          } else {
-            cardMap.set(key, {
-              bank: card.bank,
-              cardBank: card.cardBank,
-              cardType: card.cardType,
-              amount: card.amount,
-              color: cardColors[card.cardBank] || cardColors.Cash,
-              closingDate: card.closingDate ? new Date(card.closingDate.seconds * 1000) : moment().endOf('month').toDate(),
-            });
-          }
-        });
-
-        cardMap.forEach((value) => cardsData.push(value));
-      }
-      setCards(cardsData);
-    }
-    setLoading(false);
-  };
 
   useEffect(() => {
     if (!currentUser) return;
@@ -117,7 +82,17 @@ const Expenses = () => {
 
   return (
     <div style={{ padding: 24 }}>
-      <h1>Credit Cards</h1>
+      <div className='title-and-buttons' style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <h1 className='title'>Expenses</h1>
+        <div className="buttons" style={{ marginLeft: 20 }}>
+          <Button onClick={() => navigate('/detailed-expenses')} style={{ marginRight: 16 }}>
+            Detailed Expenses
+          </Button>
+          <Button onClick={() => navigate('/general-expenses')}>
+            General Expenses
+          </Button>
+        </div>
+      </div>
       <div className="cards-container" style={{ marginTop: 24 }}>
         {cards.map((card, index) => (
           <div key={index} className="card-column">
@@ -126,14 +101,6 @@ const Expenses = () => {
             </div>
           </div>
         ))}
-      </div>
-      <div style={{ marginTop: 24 }}>
-        <Button onClick={() => navigate('/detailed-expenses')} style={{ marginRight: 16 }}>
-          Detailed Expenses
-        </Button>
-        <Button onClick={() => navigate('/general-expenses')}>
-          General Expenses
-        </Button>
       </div>
     </div>
   );
